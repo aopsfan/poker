97575f0b2800ebac70ac4299cae2dbf64f2d0c1b
Bruce Ricketts <klmit1@me.com>
2013-12-25 10:11:23 -0500

    make cards comparable

--- lib/model/card.rb
+++ lib/model/card.rb
@@ -1,8 +1,21 @@
 class Card
+  include Comparable
   attr_accessor :rank, :suit
+  RANKS = [:two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :jack, :queen, :king, :ace]
+  SUITS = [:spades, :hearts, :clubs, :diamonds]
   
   def initialize(rank=nil, suit=nil)
     @rank = rank
     @suit = suit
+  end
+  
+  def <=>(card)
+    value <=> card.value
+  end
+  
+  protected
+    
+  def value
+    RANKS.index(@rank)
   end
 end
--- spec/model/card_spec.rb
+++ spec/model/card_spec.rb
@@ -20,4 +20,26 @@ describe Card do
     
   end
   
+  describe "#<=>" do
+    
+    let(:ten_diamonds) {Card.new(:ten, :diamonds)}
+    let(:two_spades) {Card.new(:two, :spades)}
+    let(:ace_spades) {Card.new(:ace, :spades)}
+    let(:ten_clubs) {Card.new(:ten, :clubs)}
+    subject {ten_diamonds}
+    
+    context "when a ten is compared to a two" do
+      it {should > two_spades}
+    end
+    
+    context "when a ten is compared to an ace" do
+      it {should < ace_spades}
+    end
+    
+    context "when a ten is compared to another ten" do
+      it {should == ten_clubs}
+    end
+    
+  end
+  
 end

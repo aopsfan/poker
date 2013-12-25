7f8ca2226942419c01ba9a75f973724e86fe43a9
Bruce Ricketts <klmit1@me.com>
2013-12-25 10:53:12 -0500

    convert card to a string

--- lib/model/card.rb
+++ lib/model/card.rb
@@ -3,6 +3,8 @@ class Card
   attr_accessor :rank, :suit
   RANKS = [:two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :jack, :queen, :king, :ace]
   SUITS = [:spades, :hearts, :clubs, :diamonds]
+  STRING_RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+  STRING_SUITS = %w[S H C D]
   
   def initialize(rank=nil, suit=nil)
     @rank = rank
@@ -13,9 +15,23 @@ class Card
     value <=> card.value
   end
   
+  def to_s
+    "#{rank_string}#{suit_string}"
+  end
+  
   protected
     
   def value
     RANKS.index(@rank)
+  end
+  
+  private
+  
+  def rank_string
+    STRING_RANKS[value]
+  end
+  
+  def suit_string
+    STRING_SUITS[SUITS.index(@suit)] # gag
   end
 end
--- spec/model/card_spec.rb
+++ spec/model/card_spec.rb
@@ -2,44 +2,59 @@ require_relative "../spec_helper.rb"
 
 describe Card do
   
+  let(:two_spades) {Card.new(:two, :spades)}
+  let(:ten_diamonds) {Card.new(:ten, :diamonds)}
+  let(:ten_clubs) {Card.new(:ten, :clubs)}
+  let(:ace_spades) {Card.new(:ace, :spades)}
+  
   describe "#new" do
-    
     context "when an ace of spades is created" do
-      let(:ace_spades) {Card.new(:ace, :spades)}
       subject {ace_spades}
       
       it "should be an ace" do
-        subject.rank.should == :ace
+        expect(subject.rank).to eq :ace
       end
       
       it "should be a spade" do
-        subject.suit.should == :spades
+        expect(subject.suit).to eq :spades
       end
-      
     end
-    
   end
   
   describe "#<=>" do
-    
-    let(:ten_diamonds) {Card.new(:ten, :diamonds)}
-    let(:two_spades) {Card.new(:two, :spades)}
-    let(:ace_spades) {Card.new(:ace, :spades)}
-    let(:ten_clubs) {Card.new(:ten, :clubs)}
     subject {ten_diamonds}
     
-    context "when a ten is compared to a two" do
-      it {should > two_spades}
+    it "should be greater than a two" do
+      should > two_spades
     end
     
-    context "when a ten is compared to an ace" do
-      it {should < ace_spades}
+    it "should be less than an ace" do
+      should < ace_spades
     end
     
-    context "when a ten is compared to another ten" do
-      it {should == ten_clubs}
+    it "should be equal to another ten" do
+      should == ten_clubs
+    end
+  end
+  
+  describe "#to_s" do
+    describe "two of spades" do
+      it "should output 2S" do
+        expect(two_spades.to_s).to match "2S"
+      end
     end
     
+    describe "ten of diamonds" do
+      it "should output 10D" do
+        expect(ten_diamonds.to_s).to match "10D"
+      end
+    end
+    
+    describe "ace of spades" do
+      it "should output AS" do
+        expect(ace_spades.to_s).to match "AS"
+      end
+    end
   end
   
 end

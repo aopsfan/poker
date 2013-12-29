3ec0175d32f7f7673107e5b8b44898b3354e945d
Bruce Ricketts <klmit1@me.com>
2013-12-29 13:22:19 -0500

    add FourOfAKind hand

---
+++ lib/model/hands/four_of_a_kind.rb
@@ -0,0 +1,26 @@
+class FourOfAKind < Hand
+  attr_accessor :set
+  
+  def self.rank
+    :four_of_a_kind
+  end
+  
+  def self.generate(cards)
+    hand = ThreeOfAKind.new
+    
+    ranks = cards.map{|card| card.rank}
+    trips_ranks = ranks.select{|rank| ranks.count(rank) == 4}
+    hand.set = cards.select{|card| trips_ranks.include? card.rank}
+    hand.kickers = cards.reject{|card| hand.set.include? card}
+    
+    hand.set.length == 4 ? hand : nil
+  end
+  
+  def to_s
+    "Quads (#{@set.first.capitalized_rank.pluralize})"
+  end
+  
+  def compare_same_rank(one_pair)
+    @set.first <=> one_pair.set.first
+  end
+end
--- spec/model/hand_specs/four_of_a_kind_spec.rb
+++ spec/model/hand_specs/four_of_a_kind_spec.rb
@@ -1,97 +1,112 @@
 require_relative "../../spec_helper.rb"
 
-describe HighCard do
+describe FourOfAKind do
   
-  let(:queen_high) do
-    hand = HighCard.new
-    hand.high = Card.new(:queen, :clubs)
+  let(:quads_queens) do
+    hand = FourOfAKind.new
+    hand.set = [Card.new(:queen, :clubs), Card.new(:queen, :hearts), Card.new(:queen, :diamonds), Card.new(:queen, :spades)]
     return hand
   end
   
-  let(:ace_high) do
-    hand = HighCard.new
-    hand.high = Card.new(:ace, :spades)
+  let(:quads_aces) do
+    hand = FourOfAKind.new
+    hand.set = [Card.new(:ace, :spades), Card.new(:ace, :diamonds), Card.new(:ace, :hearts), Card.new(:ace, :clubs)]
     return hand
   end
   
-  let(:ten_high) do
-    hand = HighCard.new
-    hand.high = Card.new(:ten, :hearts)
-    return hand
-  end
-  
-  let(:queen_high_2) do
-    hand = HighCard.new
-    hand.high = Card.new(:queen, :spades)
+  let(:quads_tens) do
+    hand = FourOfAKind.new
+    hand.set = [Card.new(:ten, :hearts), Card.new(:ten, :clubs), Card.new(:ten, :spades), Card.new(:ten, :diamonds)]
     return hand
   end
   
   describe "#generate" do
     let(:generated_hand) do
-      HighCard.generate [Card.new(:two, :diamonds), Card.new(:three, :spades),
-                         Card.new(:four, :clubs), Card.new(:five, :hearts),
-                         Card.new(:ace, :spades)]
+      FourOfAKind.generate [Card.new(:five, :diamonds), Card.new(:three, :spades),
+                            Card.new(:five, :clubs), Card.new(:five, :hearts),
+                            Card.new(:five, :spades)]
     end
     
-    context "when hand is generated" do
-      describe "high" do
-        subject {generated_hand.high}
+    let(:bad_generated_hand) do
+      FourOfAKind.generate [Card.new(:two, :diamonds), Card.new(:three, :spades),
+                            Card.new(:four, :clubs), Card.new(:five, :hearts),
+                            Card.new(:ace, :spades)]
+    end
+    
+    let(:bad_generated_hand_2) do
+      FourOfAKind.generate [Card.new(:five, :diamonds), Card.new(:three, :spades),
+                            Card.new(:four, :clubs), Card.new(:five, :hearts),
+                            Card.new(:five, :spades)]
+    end
+    
+    context "when good hand is generated" do
+      describe "set" do
+        subject {generated_hand.set}
         
-        it "should be an ace" do
-          expect(subject.rank).to eq :ace
+        it "should have 4 cards" do
+          expect(subject.length).to eq 4
         end
     
-        it "should be a spade" do
-          expect(subject.suit).to eq :spades
+        it "should be Fives" do
+          expect(subject[0].rank).to eq :five
+          expect(subject[1].rank).to eq :five
+          expect(subject[2].rank).to eq :five
+          expect(subject[3].rank).to eq :five
         end
       end
       
       describe "kickers" do
         subject {generated_hand.kickers}
         
-        it "should have 4 objects" do
-          expect(subject.length).to eq 4
+        it "should have 1 object" do
+          expect(subject.length).to eq 1
         end
         
-        it "should not have the high" do
-          expect(subject.include? generated_hand.high).to be_false
+        it "should not have the set" do
+          expect(subject.include? generated_hand.set[0]).to be_false
+          expect(subject.include? generated_hand.set[1]).to be_false
+          expect(subject.include? generated_hand.set[2]).to be_false
+          expect(subject.include? generated_hand.set[3]).to be_false
         end
+      end
+    end
+    
+    context "when bad hand is generated" do
+      it "should be nil" do
+        expect(bad_generated_hand).to be_nil
+        expect(bad_generated_hand_2).to be_nil
       end
     end
   end
   
   describe "#<=>" do
-    describe "Queen high" do
-      it "should be less than Ace high" do
-        expect(queen_high < ace_high).to be_true
+    describe "Quads (Queens)" do
+      it "should be less than Quads (Aces)" do
+        expect(quads_queens < quads_aces).to be_true
       end
       
-      it "should be greater than Ten high" do
-        expect(queen_high > ten_high).to be_true
-      end
-      
-      it "should be equal to other Queen high" do
-        expect(queen_high == queen_high_2).to be_true
+      it "should be greater than Quads (Tens)" do
+        expect(quads_queens > quads_tens).to be_true
       end
     end
   end
   
   describe "#to_s" do
-    describe "hand with high card QC" do
-      it "should output 'Queen high'" do
-        expect(queen_high.to_s).to eq "Queen high"
+    describe "hand with three Qs" do
+      it "should output 'Quads (Queens)'" do
+        expect(quads_queens.to_s).to eq "Quads (Queens)"
       end
     end
     
-    describe "hand with high card AS" do
-      it "should output 'Ace high'" do
-        expect(ace_high.to_s).to eq "Ace high"
+    describe "hand with three As" do
+      it "should output 'Quads (Aces)'" do
+        expect(quads_aces.to_s).to eq "Quads (Aces)"
       end
     end
     
-    describe "hand with high card TH" do
-      it "should output 'Ten high'" do
-        expect(ten_high.to_s).to eq "Ten high"
+    describe "hand with three 10s" do
+      it "should output 'Quads (Tens)'" do
+        expect(quads_tens.to_s).to eq "Quads (Tens)"
       end
     end
   end
